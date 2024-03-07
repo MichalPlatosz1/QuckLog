3552fd386ab622b881ff99fb63e49c97d51cdffb
PlatoszMichal1 <platosz.michal@gmail.com>
2024-03-07 19:51:44 +0100

    New config file, added format custom format options and improved inner workings

--- quicklog.js
+++ quicklog.js
@@ -2,12 +2,11 @@ const log = console.log;
 const fs = require("fs");
 
 Quicklog = {
-  options: {
-    addTimestamp: true,
+  config: {
+    format: `ql_timestamp_ISO   ||   ql_params`,
     writeToFile: {
       enabled: true,
       logPaths: ["TestLog.log"],
-      writeObjects: true,
     },
   },
 };
@@ -15,40 +14,48 @@ Quicklog = {
 console.log = function () {
   const args = Array.from(arguments);
 
-  if (Quicklog.options.addTimestamp) {
-    const event_date = new Date();
-    args.unshift(`${event_date.toISOString()}   ||  `);
+  //Applying proper format and type conversion
+  let Format = Quicklog.config.format;
+
+  //ql_params
+  let message = "";
+  args.map((param) => {
+    message += typeof param === "object" ? JSON.stringify(`${param} `) : `${param} `;
+  });
+  Format = Format.replace("ql_params", message);
+
+  //ql_timestamp
+  const event_date = new Date();
+  Format = Format.replace("ql_timestamp_ISO", event_date.toISOString());
+  Format = Format.replace("ql_timestamp", event_date);
+
+  //ql_param[0-9]
+  for (let i = 0; i < args.length; i++) {
+    Format = Format.replace(
+      `ql_param${i}`,
+      args[i] ? (typeof args[i] === "object" ? JSON.stringify(args[i]) : args[i]) : ""
+    );
   }
 
-  if (Quicklog.options.writeToFile.enabled) {
-    // Writing to all given paths
-    for (let i = 0; i < Quicklog.options.writeToFile.logPaths.length; i++) {
-      const path = Quicklog.options.writeToFile.logPaths[i];
+  //Checking for exclusion:
+  for (let i = 0; i < 10; i++) {
+    const element = args[i];
 
-      for (let j = 0; j <= args.length; j++) {
-        //Check for end of the line
-        if (j == args.length) {
-          fs.appendFileSync(path, "\n");
-          break;
-        }
-
-        let element = args[j];
-
-        //Checking type of arg in order to only write proper data type to file
-        if (typeof element !== "string") {
-          if (Quicklog.options.writeToFile.writeObjects && typeof element === "object") {
-            element = JSON.stringify(element);
-          } else {
-            continue;
-          }
-        }
-
-        fs.appendFileSync(path, `${element} `);
-      }
+    if (typeof element === "object" && element.exclude === true) {
+      args.splice(i, 1);
+      log.apply(console, [Format]);
+      return;
     }
   }
 
-  log.apply(console, args);
+  //Writing to a file
+  if (Quicklog.config.writeToFile.enabled) {
+    Quicklog.config.writeToFile.logPaths.map((path) => {
+      fs.appendFileSync(path, `${Format}\n`);
+    });
+  }
+
+  log.apply(console, [Format]);
 };
 
 module.exports = console.log;
